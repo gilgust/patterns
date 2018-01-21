5920f6df666a027722d90f4790f92a65601f80e1
gilgust <gilgust090891@gmail.com>
2018-01-21 12:54:30 +0300

    there is same problem

--- objects_pool/Program.cs
+++ objects_pool/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; 
 
 namespace objects_pool
 {
@@ -10,6 +6,34 @@ namespace objects_pool
     {
         static void Main(string[] args)
         {
+
+             var x = new RandomNumbersPool(3, 1);
+             RandomNumber first = null;
+            
+            try
+            {
+                Console.WriteLine((first = x.GetNumber()).Value);
+                Console.WriteLine(x.GetNumber().Value);
+                Console.WriteLine(x.GetNumber().Value);
+                Console.WriteLine(x.GetNumber().Value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                x.ReleaseNumber(first);
+                Console.WriteLine(x.GetNumber().Value);
+                Console.WriteLine(x.GetNumber().Value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+             
+                Console.ReadKey();
         }
     }
 }
---
+++ objects_pool/RandomNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace objects_pool
+{
+    class RandomNumber
+    {
+        private static readonly Random Rand = new Random(DateTime.Now.Minute);
+        public bool InUse { get; set; }
+
+        public BigInteger Value { get; private set; }
+
+
+        public RandomNumber()
+        {
+            var number = (BigInteger)Rand.Next(100,10000);
+            Value = Factorial(number);
+            InUse = false;
+
+        }
+
+
+     
+        private BigInteger Factorial(BigInteger input)
+        {
+            BigInteger result = 1;
+            for (int i = 2; i <= input; ++i)
+                result *= i;
+            return result;
+        }
+
+    }
+}
