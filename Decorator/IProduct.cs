d71abf0a23ce01d15caf2f0d02dc8af2237221bd
Androsov Aleksandr <gilgust090891@gmail.com>
2018-01-22 19:18:19 +0300

    some change

---
+++ Decorator/IProduct.cs
@@ -0,0 +1,12 @@
+﻿namespace Decorator
+{
+    interface IProduct
+    {
+        string Name { get; set; }
+        string Article { get; set; }
+        decimal Price { get; set; }
+
+        IProduct GetProduct();
+        void GetInfo();
+    }
+}
---
+++ Decorator/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator 
+{
+    class Product : IProduct
+    {
+        public string Name { get; set ; }
+        public string Article { get; set; }
+        public decimal Price { get; set; }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("{0} {1} {2}", Name, Article, Price);
+        }
+
+        public IProduct GetProduct()
+        {
+            Product a = new Product();
+            a.Name = this.Name;
+            a.Article = this.Article;
+            a.Price = this.Price;
+            return a;
+        }
+         
+
+    }
+}
