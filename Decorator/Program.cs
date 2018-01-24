f263ed8d8df5ff4d01edd0611871a4be5dd54be4
gilgust <gilgust090891@gmail.com>
2018-01-24 18:07:14 +0300

    Merge branch 'Decorator' into Behavioral_Patterns

---
+++ Decorator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product a = new Product() { Article = "112a", Name = "knife", Price = 2 };
+            IProduct forEU = new ProductForEU(a);
+            forEU.GetInfo();
+            IProduct forRB = new ProductForRB(a);
+            forRB.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
