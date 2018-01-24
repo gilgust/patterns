0616bfd3c64cf472b198a4e040ee74ff22213364
gilgust <gilgust090891@gmail.com>
2018-01-24 15:31:55 +0300

    done

---
+++ TemplateMethod/ProcessAducation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    abstract class ProcessAducation
+    {
+        public abstract void Enter();
+
+        public abstract void Study();
+
+        public abstract void PassExams();
+
+        public abstract void GetAttestat();
+
+        public void Process_of_Aducation()
+        {
+            Enter();
+            Study();
+            PassExams();
+            GetAttestat();
+        }
+    }
+}
---
+++ TemplateMethod/University.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class University: ProcessAducation
+    {
+        public override void Enter()
+        {
+            Console.WriteLine("Подать документы.");
+            Console.WriteLine("Вступительные экзамены.");
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("Обязательная сдача лаб. работ, экзаменовб курсовых");
+        }
+
+        public override void PassExams()
+        {
+            Console.WriteLine("Защита дипломной работы");
+        }
+
+        public override void GetAttestat()
+        {
+            Console.WriteLine("Диплом от оконцании данного университета");
+        }
+    }
+}
