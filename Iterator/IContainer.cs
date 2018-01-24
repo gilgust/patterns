1cef7135b9c7bdd90a193bc8c86251476410250a
gilgust <gilgust090891@gmail.com>
2018-01-24 16:28:18 +0300

    done

---
+++ Iterator/IContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace Iterator
+{
+    // интерфейс коллекции задаёт способ получения итератора
+    public interface IContainer<T>
+    {
+        IIterator<T> CreateIterator();
+    }
+}
---
+++ Iterator/IIterator.cs
@@ -0,0 +1,13 @@
+﻿namespace Iterator
+{
+    public interface IIterator<out T>
+    { 
+        void Reset();
+         
+        void MoveNext();
+         
+        bool IsDone { get; }
+         
+        T Current { get; }
+    }
+}
