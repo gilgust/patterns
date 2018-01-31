0b2fedac9ec837b7179cd169086251119ced8b7a
gilgust <gilgust090891@gmail.com>
2018-01-31 22:33:46 +0300

    done

---
+++ HTMLreader/Component.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace HTMLreader
+{
+    class Component: ICompanent
+    {
+        private HtmlNode _htmlComponent;
+        private List<MainPage_2> _htmlDocuments;
+
+        public Component(HtmlNode node)
+        {
+            _htmlComponent = node;
+            _htmlDocuments = new List<MainPage_2>();
+
+            var listNods = _htmlComponent.ChildNodes;
+            HtmlDocument doc;
+            string path;
+            foreach (var item in listNods)
+            {
+                if (item.Name == "a")
+                {
+                    path = item.Attributes["href"].Value;
+                    _htmlDocuments.Add(new MainPage_2(path));
+                }
+            }
+        }
+
+        public void Display()
+        {
+            var content = _htmlComponent.WriteContentTo();
+            content = Regex.Replace(content, "<.*?>", String.Empty);
+            Console.WriteLine(content);
+
+            foreach (var item in _htmlDocuments)
+            {
+                 item.Display();   
+            }
+        }
+    }
+}
--- HTMLreader/Program.cs
+++ HTMLreader/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; 
 
 namespace HTMLreader
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            MainPage_2 doc = new MainPage_2("mainPage.html"); 
+            doc.Display();
+
+            Console.ReadKey();
+
         }
     }
 }
