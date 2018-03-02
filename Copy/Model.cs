c454ba2ca75e77a057fdb15dd2bcc3aa4433da47
gilgust <gilgust090891@gmail.com>
2018-03-02 15:50:18 +0300

    progressBar_wirking

--- Copy/Model.cs
+++ Copy/Model.cs
@@ -13,42 +13,37 @@ namespace Copy
 {
     class Model: INotifyPropertyChanged
     {
-        private string _path;
-        private string _targetPath;
+        private string _sourcePath;
+        private string _targetPath; 
+        private string _directoryPath;
+        private Task _copyTask;
+        private Progress<int> _progress;
+        private int _progressCount;
 
-        private Thread _readerThread;
-        private Thread _writerThread;
-        private readonly Queue<byte[]> _queueBytes;
-        //private bool _readyForCopy;
-        private double _progress;
-
-        
 
         public Model()
-        {
-            _queueBytes = new Queue<byte[]>();
-            _path = String.Empty;
+        { 
+            _sourcePath = String.Empty;
             _targetPath = String.Empty;
-            _progress = 0;
+            _progress = new Progress<int>(procent => Progress = procent);
         }
 
-        public double Progress
+        public int Progress
         {
-            get => _progress;
+            get => _progressCount;
             set
             {
-                _progress = value; 
+                _progressCount = value;
                 OnPropertyChanged(nameof(Progress));
             }
         }
 
-        public string Path
+        public string SourcePath
         {
-            get => _path;
+            get => _sourcePath;
             set
             {
-                _path = value;
-                //ReadinessTest();
+                _sourcePath = value; 
                 OnPropertyChanged(nameof(Path)); 
             }
         }
@@ -58,93 +53,55 @@ namespace Copy
             get => _targetPath;
             set
             {
-                _targetPath = value;
-                //ReadinessTest();
+                _targetPath = value; 
                 OnPropertyChanged(nameof(TargetPath)); 
             }
         }
 
-        // here i had tried to make CopyButton not available
-
-        //public bool ReadyForCopy {
-        //    get => _readyForCopy;
-        //    set
-        //    {
-        //        _readyForCopy = value;
-        //        OnPropertyChanged(nameof(ReadyForCopy));
-        //    }
-        //}
-        //public void ReadinessTest()
-        //{
-        //    if (!File.Exists(_path) || File.Exists(_targetPath))
-        //        _readyForCopy = false;
-        //    else
-        //    {
-        //        _readyForCopy = true;
-        //    }
-        //}
+        public string DirectoryPath
+        {
+            get => _directoryPath;
+            set
+            {
+                _directoryPath = value;
+                OnPropertyChanged(nameof(DirectoryPath));
+            }
+        }
 
         public void Copy()
         {
-            if(File.Exists(TargetPath) || !File.Exists(Path))
+            if (File.Exists(TargetPath) || !File.Exists(SourcePath))
                 return;
-            _readerThread = new Thread(ReadFile);
-            _writerThread = new Thread(WriteFile);
-
-            _readerThread.Start();
-            _writerThread.Start();
+            _copyTask = Task.Run(() => CopyMethod());
 
 
         }
 
-        private void ReadFile()
+        private void CopyMethod()
         {
-            using (FileStream reader = new FileStream(_path, FileMode.Open, FileAccess.Read))
+            using (FileStream reader = new FileStream(_sourcePath, FileMode.Open, FileAccess.Read))
+            using (FileStream writer = new FileStream(_targetPath, FileMode.Create, FileAccess.Write))
             {
+                var percentOfFileSize = (int)reader.Length / 100;
+                var counter = 0;
+                var compleatedProcent = 0;
                 int bufferSize = 4096;
 
                 while (reader.Position < reader.Length)
                 {
-                    if (_queueBytes.Count < 256)
-                    {
-                        var arrayBytes = new byte[bufferSize];
-                        reader.Read(arrayBytes, 0, bufferSize);
-                        _queueBytes.Enqueue(arrayBytes);
-                        Progress += reader.Length / bufferSize;
-                    }
-                    else
-                    {
-                        Thread.Sleep(20);
-                    }
+                    var arrayBytes = new byte[bufferSize];
+                    reader.Read(arrayBytes, 0, bufferSize);
+                    writer.Write(arrayBytes, 0, bufferSize);
+
+                    counter += bufferSize;
+                    if (counter > compleatedProcent * percentOfFileSize)
+                        ((IProgress<int>)_progress).Report(compleatedProcent++);
+
+                    ((IProgress<int>)_progress).Report(100);
                 }
             }
         }
 
-        private void WriteFile()
-        {
-            using (FileStream writer = new FileStream(_targetPath, FileMode.Create, FileAccess.Write))
-            {
-                int bufferSize = 4096;
-                var arrayBytes = new byte[bufferSize];
-                while (_queueBytes.Count > 0 || _readerThread.IsAlive)
-                {
-                    if (_queueBytes.Count > 0)
-                    {
-                        arrayBytes = _queueBytes.Dequeue();
-                        if (arrayBytes == null)
-                            continue;
-                        writer.Write(arrayBytes, 0, bufferSize);
-                    }
-                    else
-                    {
-                        Thread.Sleep(20);
-                    }
-                }
-
-                Progress = 0;
-            }
-        }
-        
 
 
 
