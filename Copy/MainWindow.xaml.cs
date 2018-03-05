6bf1c628a54db7e17f808ec98c7da65880c506bc
Androsov Aleksandr <gilgust090891@gmail.com>
2018-03-05 19:32:18 +0300

    start

--- Copy/MainWindow.xaml.cs
+++ Copy/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using Application = System.Windows.Forms.Application;
 
 namespace Copy
 {
@@ -20,9 +23,56 @@ namespace Copy
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Model _model; 
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _model = new Model();
+            this.DataContext = _model;
+        }
+
+
+        //select file
+        private void Path_OnClick(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog
+            {
+                InitialDirectory = Application.StartupPath
+            };
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                _model.SourcePath = ofd.FileName;
+        }
+
+        //select directory
+        private void DirectoryPathButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                _model.SourcePath = fbd.SelectedPath;
+        }
+
+        //set target path for File
+        private void TargetPath_OnClick(object sender, RoutedEventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                _model.TargetPath = sfd.FileName;
+        }
+
+        //set target path for Directory
+        private void TargetDirectoryPathButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                _model.TargetPath = fbd.SelectedPath;
+        }
+
+        // start copy
+        private void CopyButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _model.Copy();
         }
     }
 }
